76a6fbf7382bf96c66ee84e4e55961121e0d0505
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-07-12 19:06:01 -0300

    [master/api] feat: Extrai schema/query/mutation do objeto Categoria para arquivos próprios

---
+++ api/src/Categoria/index.js
@@ -0,0 +1,9 @@
+const Categoria = require('./schema.js');
+// const CategoriaQuery = require('./query.js');
+const CategoriaMutation = require('./mutation.js');
+
+module.exports = {
+	Categoria,
+	// CategoriaQuery,
+	CategoriaMutation,
+};
