373d77c308b57c6a398592eccaeef4b3e92b66bd
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-07-18 00:49:38 -0300

    [master/web] feat: NavigationBar iniciada

---
+++ web/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// CSS
+import * as s from './NavigationBar.style';
+
+// UI
+import { Container, Row, Column } from '../../ui-components';
+
+const NavigationBar = () => (
+	<s.NavigationBar>
+		<Container>
+			<Row>
+				<Column>a</Column>
+			</Row>
+		</Container>
+	</s.NavigationBar>
+);
+
+export default NavigationBar;
---
+++ web/components/NavigationBar/NavigationBar.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const NavigationBar = styled.div`
+	height: 81px;
+	border-bottom: 1px solid #F0F0F0;
+`;
