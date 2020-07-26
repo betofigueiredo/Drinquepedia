e459ec3fee6aa070aa5cf0e26c9eb9b3e5dd562c
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-07-26 19:20:40 -0300

    [master/web] feat: Mostra destaques na home

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,10 +1,17 @@
 import { Container, Row, Column } from '../ui-components';
 import NavigationBar from '../components/NavigationBar';
+import Destaques from '../components/Destaques';
 
 const Home = () => (
 	<>
 		<NavigationBar />
 		<Container>
+			<Row>
+				<Column>
+					<Destaques />
+				</Column>
+			</Row>
+
 			<Row>
 				<Column>aaa</Column>
 				<Column>aaa</Column>
