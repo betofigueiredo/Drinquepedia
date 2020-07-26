ae47ca4a7ba5e37cb95e82193749bce554e7e229
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-07-26 19:19:41 -0300

    [master/web] refactor: Adiciona max_width do theme

--- web/ui-components/Container/Container.style.js
+++ web/ui-components/Container/Container.style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import theme from '../../styles/theme';
 
 export const Container = styled.div`
-	max-width: 1200px;
+	max-width: ${theme.max_width}px;
 	margin: 0 auto;
 `;
