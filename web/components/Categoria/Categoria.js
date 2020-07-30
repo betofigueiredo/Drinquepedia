959d19e9ca41fca88c8ec915db01653119de1878
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-07-29 22:47:29 -0300

    [master/web] feat: Testes com requests iniciais

--- web/components/Categoria/Categoria.js
+++ web/components/Categoria/Categoria.js
@@ -1,27 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { gql } from '@apollo/client';
 
 import client from '../../services';
 
-const Categoria = () => {
-	useEffect(() => {
-		client
-			.query({
-				query: gql`
-					query GetCategorias {
-						categorias {
-							id
-							nome
-						}
-					}
-				`
-			})
-			.then(result => console.log(result));
-	}, []);
+const query = gql`
+    query GetCategorias {
+        categorias {
+            id
+            nome
+        }
+    }
+`;
 
-	return (
-		<div>aaaa</div>
-	);
+const Categoria = () => {
+    const [categorias, setCategorias] = useState([]);
+
+    useEffect(() => {
+        client
+            .query({ query })
+            .then(result => {
+                setCategorias(result.data.categorias || []);
+            });
+    }, []);
+
+    return (
+        <div>
+            {categorias.map(categoria => (
+                <p key={categoria.id}>{categoria.nome}</p>
+            ))}
+        </div>
+    );
 };
 
 export default Categoria;
