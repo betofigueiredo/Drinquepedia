86e3ff7695ab025bac6150871101e17e220a6200
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-07-29 22:49:24 -0300

    [master/web] wip: Page Drinques criada

---
+++ web/pages/drinques.js
@@ -0,0 +1,20 @@
+import { Container, Row, Column } from '../ui-components';
+import NavigationBar from '../components/NavigationBar';
+import Categoria from '../components/Categoria';
+
+const Drinques = () => (
+	<>
+		<NavigationBar />
+		<Container>
+			<Row>
+				<Column>aaa</Column>
+				<Column>aaa</Column>
+				<Column>aaa</Column>
+			</Row>
+
+			<Categoria />
+		</Container>
+	</>
+);
+
+export default Drinques;
