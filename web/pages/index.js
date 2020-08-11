cdaf08457732cb9b44a2a09e04ba7c52b1cd7a4e
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-08-10 23:26:36 -0300

    [master/web] feat: Next head to add meta tags and title

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import Destaques from 'components/Destaques';
 import { Container, Row, Column } from 'ui-components';
 
 const Home = () => (
     <>
+        <Head>
+            <title>Drinquepedia</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <Container>
             <Row>
                 <Column>
