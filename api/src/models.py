416b9ba14a2e3d753fedee58d3fa82dee3da510c
Beto <beto@2sidesof1.com>
2024-03-10 19:05:20 -0300

    feat(all): updates

--- api/src/models.py
+++ api/src/models.py
@@ -23,7 +23,7 @@ class Drink(db.Model):
 class Ingredient(db.Model):
     id = db.Column(db.String(36), primary_key=True, index=True, default=uuid.uuid4)
     order = db.Column(TINYINT, nullable=False)
-    quantity = db.Column(SMALLINT)
+    quantity = db.Column(db.String(50))
     unit_of_measurement = db.Column(db.String(36))
     ingredient_type_id = db.Column(db.String(36), db.ForeignKey("ingredient_type.id"))
     ingredient_type = relationship("IngredientType", back_populates="ingredients")
