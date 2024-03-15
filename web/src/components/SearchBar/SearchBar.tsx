31281e2bcbdb3d2545e9e733c6e64b75097a4801
Beto <beto@2sidesof1.com>
2024-03-15 18:21:54 -0300

    feat(all): updates

--- web/src/components/SearchBar/SearchBar.tsx
+++ web/src/components/SearchBar/SearchBar.tsx
@@ -3,7 +3,7 @@ import CaloriesSelect from "./components/CaloriesSelect";
 import AlcoholicContentSelect from "./components/AlcoholicContentSelect";
 
 const SearchBar = () => (
-  <div className="pt-8 pb-10 mb-14 bg-gray-50 border-b border-slate-200">
+  <div className="pt-8 pb-10 bg-gray-50 border-b border-slate-200">
     <div className="container grid grid-flow-col gap-6 justify-start">
       <SearchInput />
       <CaloriesSelect />
--- web/src/pages/Drinks/Drinks.tsx
+++ web/src/pages/Drinks/Drinks.tsx
@@ -4,6 +4,7 @@ import SearchBar from "@/components/SearchBar";
 import useSearchStore from "@/stores/useSearchStore";
 import DrinkRow from "@/components/DrinkRow";
 import Pagination from "@/components/Pagination";
+import HighlightCard from "@/components/HighlightCard";
 
 const Drinks = ({ category }: { category?: string }) => {
   const PER_PAGE = 20;
@@ -28,14 +29,14 @@ const Drinks = ({ category }: { category?: string }) => {
         {isPending && <div>Carregando...</div>}
         {error && <div>Erro</div>}
         {!isPending && !error && (
-          <>
-            <div>Destaques laterais (caipirinhas e smoothies)</div>
+          <div className="grid grid-cols-[auto,313px] gap-4">
             <div>
+              <h1 className="font-serif font-bold text-3xl text-gray-700 mt-14 mb-14">
+                {category || "Todos os drinks"}
+              </h1>
               {drinks?.map((drink) => (
                 <DrinkRow key={drink.id} drink={drink} />
               ))}
-            </div>
-            <div>
               <Pagination
                 category={category}
                 page={page}
@@ -43,7 +44,20 @@ const Drinks = ({ category }: { category?: string }) => {
                 totalCount={data?.metadata?.totalCount || 0}
               />
             </div>
-          </>
+            <div className="mt-24">
+              <HighlightCard
+                highlight={{
+                  id: "6",
+                  oldId: 6,
+                  type: "GENERAL",
+                  title: "TOP 10 TROPICAIS",
+                  subtitle:
+                    "Confira nossa lista dos 10 melhores drinques tropicais para esse verão. Não deixe de experimentar. Confira já e divirta-se!",
+                  description: "",
+                }}
+              />
+            </div>
+          </div>
         )}
       </div>
     </>
