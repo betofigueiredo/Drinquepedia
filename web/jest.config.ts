3a71cc4639ec15ebf7b8aced59cf53147b68d398
Beto <beto@2sidesof1.com>
2024-03-16 19:23:24 -0300

    feat(web): tests for web

---
+++ web/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+  },
+};
+
+export default config;
