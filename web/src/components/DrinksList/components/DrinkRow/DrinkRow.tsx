d2ae8d32268dae7db1034c3f6dfe11dcbc846715
Beto <beto@2sidesof1.com>
2024-03-17 12:33:10 -0300

    feat(web): drink row hover style

--- web/src/components/DrinksList/components/DrinkRow/DrinkRow.tsx
+++ web/src/components/DrinksList/components/DrinkRow/DrinkRow.tsx
@@ -9,14 +9,15 @@ const DrinkRow = ({ drink }: { drink: Drink }) => {
   return (
     <Link to={`/drinques/${drink.oldId}`} className="group">
       <div className="grid grid-cols-[max-content_1fr] mb-7 text-slate-950">
-        <div className="mr-6 w-16 h-16 border-[5px] group-hover:border-amber-300 transition-all duration-200">
+        <div className="mr-6 w-16 h-16 border-[5px] overflow-hidden ">
           <img
             src={`/images/drinks/${drink?.oldId}/${drink?.oldId}p.jpg`}
+            className="group-hover:opacity-90 transition-all duration-200 group-hover:scale-110"
             width="64"
           />
         </div>
         <div>
-          <h1 className="font-serif font-bold text-2xl text-amber-500 group-hover:text-amber-300">
+          <h1 className="font-serif font-bold text-2xl text-amber-500 group-hover:text-amber-300 group-hover:underline">
             {drink?.name}
           </h1>
           <div className="text-gray-500">{ingredients}</div>
