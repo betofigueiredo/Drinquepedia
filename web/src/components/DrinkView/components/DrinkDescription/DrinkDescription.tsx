5227053632a34f9ecad8f6f221459d766e53beda
Beto <beto@2sidesof1.com>
2024-03-17 16:17:08 -0300

    fix(web): do not show empty drink description

--- web/src/components/DrinkView/components/DrinkDescription/DrinkDescription.tsx
+++ web/src/components/DrinkView/components/DrinkDescription/DrinkDescription.tsx
@@ -1,17 +1,23 @@
 import { Drink } from "@/types/drink";
 
-const DrinkDescription = ({ drink }: { drink?: Drink }) => (
-  <div>
-    <h5 className="font-serif font-bold text-2xl text-slate-950 mb-3">
-      Sobre o drinque
-    </h5>
-    <div className="text-slate-950 pl-8 relative whitespace-pre-line">
-      <div className="absolute top-0 left-0 font-serif font-bold text-5xl text-amber-500 mb-3">
-        “
+const DrinkDescription = ({ drink }: { drink?: Drink }) => {
+  if (!drink?.description) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h5 className="font-serif font-bold text-2xl text-slate-950 mb-3">
+        Sobre o drinque
+      </h5>
+      <div className="text-slate-950 pl-8 relative whitespace-pre-line">
+        <div className="absolute top-0 left-0 font-serif font-bold text-5xl text-amber-500 mb-3">
+          “
+        </div>
+        {drink?.description}
       </div>
-      {drink?.description}
     </div>
-  </div>
-);
+  );
+};
 
 export default DrinkDescription;
