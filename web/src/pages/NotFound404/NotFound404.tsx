dbd1fcdee1d30d8b0e79331c30d004a3ca73daaf
Beto <beto@2sidesof1.com>
2024-03-17 16:59:30 -0300

    feat(web): 404 page

--- web/src/pages/NotFound404/NotFound404.tsx
+++ web/src/pages/NotFound404/NotFound404.tsx
@@ -1,5 +1,19 @@
 const NotFound404 = () => {
-  return <div className="container">NotFound404</div>;
+  return (
+    <div className="container">
+      <h1 className="font-serif font-bold text-[128px] text-amber-300 mt-24 mb-0 leading-none">
+        404
+      </h1>
+      <h2 className="font-serif font-bold text-4xl text-gray-700 mb-4">
+        Página não encontrada!
+      </h2>
+      <p className="text-gray-700">
+        A página solicitada não pôde ser encontrada.
+        <br />
+        Utilize o menu acima para encontrar o que você procura. Obrigado!
+      </p>
+    </div>
+  );
 };
 
 export default NotFound404;
